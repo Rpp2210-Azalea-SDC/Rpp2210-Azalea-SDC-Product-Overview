e82485485a500799745c49db3ab00b59aae2677e
Alvin Ruan <caskgar@gmail.com>
2023-05-08 01:33:26 -0400

    Added query for getRelated Items

--- database/db.js
+++ database/db.js
@@ -1,40 +1,68 @@
-const postgresDB = require('./postgreSQL.js');
-
-
+const postgresDB = require("./postgreSQL.js");
 
 const getProductByID = (id, callback) => {
   const pool = postgresDB();
-  pool.query(`select * from product where id = ${id}`)
-  .then((result) => {
-    callback(null, result);
-  })
-  .catch((err) => {
-    callback(err, null);
-  })
-}
-
-const getProducts = (callback) => {
-  pool.query()
+  pool
+    .query(
+      `SELECT
+      product.*,
+      json_agg(
+        json_build_object(
+          'feature', features.feature,
+          'value', features.value
+        )
+      ) AS features
+      FROM product JOIN features ON product.product_id = features.product_id
+      WHERE product.product_id = ${id}
+      GROUP BY product.product_id
+    `
+    )
+    .then((result) => {
+      callback(null, result);
+    })
+    .catch((err) => {
+      callback(err, null);
+    });
 };
 
+const getProducts = (callback) => {
+  pool
+    .query(`select * from product limit 5`)
+    .then((result) => {
+      callback(null, result);
+    })
+    .catch((err) => {
+      callback(err, null);
+    });
+};
 
 const getStyles = (id, callback) => {
-  pool.query()
-}
+  pool.query();
+};
 
 const getFeatures = (id, callback) => {
-  pool.query()
-}
+  pool.query();
+};
 
 const getRelated = (id, callback) => {
-  pool.query()
-}
-
+  pool
+    .query(
+      `SELECT json_agg(related_id) AS related_ids
+      FROM related_items
+      WHERE product_id = ${id}`
+    )
+    .then((result) => {
+      callback(null, result);
+    })
+    .catch((err) => {
+      callback(err, null);
+    });
+};
 
 module.exports = {
   getProducts,
   getProductByID,
   getStyles,
   getFeatures,
-  getRelated
-}
+  getRelated,
+};
--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,7 @@
 const { Client } = require(`pg`);
+const getProductByID = require("./database/db.js"); // db.js called
+const getProducts = require("./database/db.js");
+
 require("dotenv").config();
 const DBpw = process.env.DBpw;
 const DBname = process.env.DBname;
@@ -8,7 +11,7 @@ describe("postgres database test", () => {
   let client;
   beforeAll(() => {
     client = new Client({
-      host: 'localhost',
+      host: "localhost",
       port: 5432,
       database: DBname,
       user: DBuser,
@@ -17,12 +20,44 @@ describe("postgres database test", () => {
     client.connect();
   });
 
-
-
   it("test if database has productoverview data", async () => {
-    const res = await client.query(`select * from product where product_id = 1`);
+    const res = await client.query(`SELECT
+    product.*,
+    json_agg(
+      json_build_object(
+        'feature', features.feature,
+        'value', features.value
+      )
+    ) AS features
+    FROM product JOIN features ON product.product_id = features.product_id
+    WHERE product.product_id = 3
+    GROUP BY product.product_id
+  `);
+    console.log(
+      "Test response select * from product where product_id = 1:",
+      res.rows
+    );
     expect(res.rows.length).toBeGreaterThan(0);
   }, 3000);
+
+  it("test returned from products to be count of 5", async () => {
+    const res = await client.query(`select * from product limit 5`);
+    console.log("Test response select * from product limit 5:", res.rows);
+    expect(res.rows.length).toBe(5);
+  });
+
+  it("tests the related items returned to be [2, 3, 8, 7]", async () => {
+    const res = await client.query(
+      `SELECT json_agg(related_id) AS related_ids
+      FROM related_items
+      WHERE product_id = 1`
+    );
+    console.log(
+      "tests the related items returned to be [2, 3, 8, 7] ===> ",
+      res.rows[0].related_ids
+    );
+    expect(res.rows[0].related_ids).toEqual([2, 3, 8, 7]);
+  });
 
   afterAll(() => {
     client.end();
